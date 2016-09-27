93ed8aee29fb64ce34c3899f7ccdb6cde0fc688d
akash0770 <sunnyohi@gmail.com>
2016-09-27 14:19:21 +1000

    Add files via upload

---
+++ daos/BookHelperTest.java
@@ -0,0 +1,28 @@
+
+package library.daos;
+
+import junit.framework.TestCase;
+
+/**
+ * @author 11540721
+ *
+ */
+public class BookHelperTest extends TestCase {
+
+	
+	protected void setUp() throws Exception {
+		super.setUp();
+	}
+
+	// test for testmakebook 
+        //.
+	public void testMakeBook() {
+		fail("Not yet implemented");
+	}
+
+	private void fail(String string) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}//.
